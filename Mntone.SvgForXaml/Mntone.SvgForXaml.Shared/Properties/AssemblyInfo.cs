93961b804af20ec4e67015e872ef646744038bc6
モノトーン <sd8@live.jp>
2015-12-24 14:42:52 +0900

    Add Windows 8.1, Windows Phone 8.1 projects.

---
+++ Mntone.SvgForXaml/Mntone.SvgForXaml.Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using Mntone.SvgForXaml.Internal;
+using System.Reflection;
+
+[assembly: AssemblyTitle(AssemblyInfo.QualifiedName)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("mntone")]
+[assembly: AssemblyProduct(AssemblyInfo.QualifiedName)]
+[assembly: AssemblyCopyright("Copyright (C) 2015- mntone")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
+
+namespace Mntone.SvgForXaml.Internal
+{
+	internal static class AssemblyInfo
+	{
+		public const string QualifiedName = "Svg for Xaml Library";
+	}
+}
